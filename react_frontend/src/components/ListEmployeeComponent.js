3c89ea3497176e935023a53e3c3a04e4b2937b35
unknown <gunaharihemanth@gmail.com>
2024-10-01 14:16:47 +0530

    react project

---
+++ react_frontend/src/components/ListEmployeeComponent.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService';
+import { Link } from 'react-router-dom';
+
+export default class ListEmployeeComponent extends Component {
+
+    constructor()
+    {
+        super();
+
+        this.state={
+            employees:[]
+        }
+    }
+
+    componentDidMount()
+    {
+        EmployeeService.getEmployees().then((res)=>
+           {
+            this.setState({employees:res.data})
+           } )
+    }
+
+    deleteEmployee=(employeeId)=>
+    {
+      EmployeeService.deleteEmployee(employeeId).then(res=>
+      {
+          EmployeeService.getEmployees().then(res=>
+          {
+              this.setState({employees:res.data})
+          }
+          )
+      })
+      .catch(error =>
+      {
+        console.log(error)
+      }
+      )
+    }
+
+
+
+
+  render() {
+    const employeeIds = [];
+    for (let i = 0; i < this.state.employees.length; i++) {
+      employeeIds.push({ ...this.state.employees[i], displayid: i + 1 });
+    }
+    return (
+      <div className='container mt-5'>
+        
+            <h4 className='text-center'>Employee List</h4>
+            <div className='row'>
+        <Link to="/add-employee" className='btn btn-primary my-4'> Add Employee</Link>
+
+
+            <table className='table table-bordered table-striped w-100'>
+                   <thead>
+                        <tr>
+                            <th> ID </th>
+                            <th> First Name</th>
+                            <th> Last Name</th>
+                            <th> Email</th>
+                            <th> Actions</th>
+                        </tr>
+                   </thead>
+                   <tbody>
+                    {
+                            employeeIds.map((employee, index) => (
+                                <tr key={index}>
+                                       <td>{employee.displayid}</td> 
+                                       <td>{employee.firstName}</td> 
+                                       <td>{employee.lastName}</td> 
+                                       <td>{employee.email}</td> 
+                                       <td>
+   <Link to={`/update-employee/${employee.id}`} className='btn btn-info'> update </Link>                        
+   <button style={{marginLeft:"10px"}} className='btn btn-danger'
+   onClick={()=> this.deleteEmployee(employee.id)}> Delete </button>
+                                       </td>
+                                </tr>
+                        ))
+                        
+                    }
+                   </tbody>
+            </table>
+            </div>
+      </div>
+    )
+  }
+}
